99d9789768a66298d0a3afdedd30d90d90c2b7c7
Kevin Gomes <kevingomes17@gmail.com>
2018-10-22 00:16:20 -0500

    + Mdae changes such that a cleaner Swagger Spec is generated. + Updated the Swagger Spec JSON as well. + Got rid of a redundant plugin in pom.xml

--- evtreg/src/main/java/com/twa/evtreg/EvtregSwaggerConfig.java
+++ evtreg/src/main/java/com/twa/evtreg/EvtregSwaggerConfig.java
@@ -2,8 +2,11 @@ package com.twa.evtreg;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,8 +18,21 @@ public class EvtregSwaggerConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.twa.evtreg.controllers"))
                 .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Event Reservation REST API")
+                .description("\"Allows you to View Availability, Book, Change & Cancel Reservations.\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Kevin Gomes", "http://kgomes.info/", "kevingomes17@gmail.com"))
                 .build();
     }
 }
--- evtreg/src/main/java/com/twa/evtreg/controllers/ReservationController.java
+++ evtreg/src/main/java/com/twa/evtreg/controllers/ReservationController.java
@@ -3,7 +3,11 @@ package com.twa.evtreg.controllers;
 import com.twa.evtreg.models.entities.Reservation;
 import com.twa.evtreg.models.dto.*;
 import com.twa.evtreg.services.ReservationService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -21,18 +25,49 @@ public class ReservationController {
      * @param req
      * @return
      */
-    @RequestMapping(value = "/available-dates", method = RequestMethod.GET, params = {"start", "end"})
+    @RequestMapping(value = "/available-dates",
+            method = RequestMethod.GET,
+            params = {"start", "end"},
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiOperation(
+            value = "Check availability",
+            notes = "Check availability either by specifying a date range. Or omitting the Date Range will fetch availability for the next 30 days."
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = AvailabilitySearchRes.class)
+    })
     public AvailabilitySearchRes getAvailableDates(AvailabilitySearchReq req) {
         List<Date> dates = service.getAvailableDates(req.getStart(), req.getEnd());
         return new AvailabilitySearchRes(req.getStart(), req.getEnd(), dates);
     }
 
-    @RequestMapping(value = "/available-dates", method = RequestMethod.GET)
+    @RequestMapping(value = "/available-dates",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiOperation(
+            value = "Check availability",
+            notes = "Check availability either by specifying a date range. Or omitting the Date Range will fetch availability for the next 30 days."
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = AvailabilitySearchRes.class)
+    })
     public AvailabilitySearchRes getAvailableDatesFor() {
         return service.getAvailableDates();
     }
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @RequestMapping(value = "/list",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiOperation(
+            value = "View Reservations",
+            notes = "Displays all the reservations made."
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = ReservationRes[].class)
+    })
     public List<ReservationRes> list() {
         List<ReservationRes> res = new ArrayList<>();
 
@@ -49,7 +84,20 @@ public class ReservationController {
      * @param req
      * @return
      */
-    @RequestMapping(value = "/book", method = RequestMethod.POST)
+    @RequestMapping(value = "/book",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiOperation(
+            value = "Book Reservation",
+            notes = "Books a reservation after all the validation criteria are met. Handles Race Condition."
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Created", response = ReservationRes.class),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "Validation failed. Dates not available."),
+            @ApiResponse(code = 406, message = "Validation failed.")
+    })
     public ReservationRes book(@Valid @RequestBody ReservationReq req) {
         service.validate(req);
         service.lock();
@@ -67,7 +115,20 @@ public class ReservationController {
      * @param req
      * @return
      */
-    @RequestMapping(value = "/book-faulty", method = RequestMethod.POST)
+    @RequestMapping(value = "/book-faulty",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiOperation(
+            value = "Book Reservation",
+            notes = "Books a reservation after all the validation criteria are met. Does not handle Race Condition."
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Created", response = ReservationRes.class),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "Validation failed. Dates not available."),
+            @ApiResponse(code = 406, message = "Validation failed.")
+    })
     public ReservationRes bookFaulty(@Valid @RequestBody ReservationReq req) {
         service.validate(req);
         Reservation res;
@@ -81,7 +142,20 @@ public class ReservationController {
      * @param req
      * @return
      */
-    @RequestMapping(value = "/change/{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/change/{id}",
+            method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiOperation(
+            value = "Change Reservation",
+            notes = "Allows you to update a reservation after all the validation criteria are met. Handle Race Condition."
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Created", response = ReservationRes.class),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "Validation failed. Reservation not found or Dates not available."),
+            @ApiResponse(code = 406, message = "Validation failed. Or No change in reservation.")
+    })
     public ReservationRes change(@PathVariable("id") Long id, @Valid @RequestBody ReservationReq req) {
         req.setId(id);
         service.validateExisting(req);
@@ -100,7 +174,18 @@ public class ReservationController {
      * @param id
      * @return
      */
-    @RequestMapping(value = "/cancel/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/cancel/{id}",
+            method = RequestMethod.DELETE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiOperation(
+            value = "Cancel Reservation",
+            notes = "Cancels a reservation."
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cancelled Reservation", response = BasicRes.class),
+            @ApiResponse(code = 404, message = "Not found")
+    })
     public BasicRes cancel(@PathVariable("id") Long id) {
         Boolean flag = service.cancel(id);
         return new BasicRes(flag, flag ? "Successfully cancelled reservation!" : "Unable to cancel reservation", null);
@@ -111,7 +196,14 @@ public class ReservationController {
      * Used to demo other APIs' availability while Book/Update Reservation might have locked access to handle race condition.
      * @return
      */
-    @RequestMapping(value = "/lock", method = RequestMethod.GET)
+    @RequestMapping(value = "/lock",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiOperation(
+            value = "Lock Reservation Booking or Changing",
+            notes = "Locks the booking or changing of any reservation. Used to demonstrate that the Check Availability continues to work even though the Reservation Booking/Updating is locked."
+    )
     public BasicRes lock() {
         service.lock();
         return new BasicRes(true, "Locked Semaphore", null);
@@ -122,7 +214,14 @@ public class ReservationController {
      * Used to demo other APIs' availability while Book/Update Reservation might have locked access to handle race condition.
      * @return
      */
-    @RequestMapping(value = "/unlock", method = RequestMethod.GET)
+    @RequestMapping(value = "/unlock",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiOperation(
+            value = "Unlock Reservation Booking or Changing",
+            notes = "Unlocks the booking or changing of any reservation."
+    )
     public BasicRes unlock() {
         service.unlock();
         return new BasicRes(true, "Unlocked Semaphore", null);
